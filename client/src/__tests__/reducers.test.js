0be1b7cfdc795b9ca25c98d9a6afdb5e06fcc451
Ben H <deepfreespace@gmail.com>
2022-05-16 17:02:43 -0600

    Reducer created, first test passed

--- client/src/__tests__/reducers.test.js
+++ client/src/__tests__/reducers.test.js
@@ -5,9 +5,22 @@ import {
     UPDATE_CURRENT_CATEGORY,
 } from '../utils/actions';
 
+// Import reducer
+import { reducer } from '../utils/reducers';
+
 // Create sample global state
 const initialState = {
     products: [],
     categories: [{ name: 'Food' }],
     currentCategory: '1',
 };
+
+test('UPDATE_PRODUCTS', () => {
+    let newState = reducer(initialState, {
+        type: 'UPDATE_PRODUCTS',
+        products: [{}, {}],
+    });
+
+    expect(newState.products.length).toBe(2);
+    expect(initialState.products.length).toBe(0);
+});
