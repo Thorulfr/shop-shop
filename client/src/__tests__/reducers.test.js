32b5abd3b15334f7cb231d286828dadcf1a94696
Ben H <deepfreespace@gmail.com>
2022-05-18 17:18:14 -0600

    Tests passing, beginning global state implementation

--- client/src/__tests__/reducers.test.js
+++ client/src/__tests__/reducers.test.js
@@ -17,10 +17,30 @@ const initialState = {
 
 test('UPDATE_PRODUCTS', () => {
     let newState = reducer(initialState, {
-        type: 'UPDATE_PRODUCTS',
+        type: UPDATE_PRODUCTS,
         products: [{}, {}],
     });
 
     expect(newState.products.length).toBe(2);
     expect(initialState.products.length).toBe(0);
+});
+
+test('UPDATE_CATEGORIES', () => {
+    let newState = reducer(initialState, {
+        type: UPDATE_CATEGORIES,
+        categories: [{}, {}],
+    });
+
+    expect(newState.categories.length).toBe(2);
+    expect(initialState.categories.length).toBe(1);
+});
+
+test('UPDATE_CURRENT_CATEGORY', () => {
+    let newState = reducer(initialState, {
+        type: UPDATE_CURRENT_CATEGORY,
+        currentCategory: '2',
+    });
+
+    expect(newState.currentCategory).toBe('2');
+    expect(initialState.currentCategory).toBe('1');
 });
--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,8 +1,10 @@
+// Imports
 import {
     UPDATE_PRODUCTS,
     UPDATE_CATEGORIES,
     UPDATE_CURRENT_CATEGORY,
 } from './actions';
+import { useReducer } from 'react';
 
 export const reducer = (state, action) => {
     switch (action.type) {
@@ -12,9 +14,22 @@ export const reducer = (state, action) => {
                 ...state,
                 products: [...action.products],
             };
-
-        // if it's none of these actions, do not update state at all and keep things the same!
+        // if action type value is the value of `UPDATE_CATEGORIES`, return a new state object with an updated categories array
+        case UPDATE_CATEGORIES:
+            return {
+                ...state,
+                categories: [...action.categories],
+            };
+        case UPDATE_CURRENT_CATEGORY:
+            return {
+                ...state,
+                currentCategory: action.currentCategory,
+            };
         default:
             return state;
     }
 };
+
+export function useProductReducer(initialState) {
+    return useReducer(reducer, initialState);
+}
