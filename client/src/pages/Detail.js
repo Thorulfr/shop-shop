700a98dd87ee064cc26eb9cfa41b2ae4ee835212
Ben H <deepfreespace@gmail.com>
2022-07-27 18:50:11 -0600

    Fixed stupid two-space indents

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -1,58 +1,54 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
-
 import { useStoreContext } from '../utils/GlobalState';
 import { UPDATE_PRODUCTS } from '../utils/actions';
 import { QUERY_PRODUCTS } from '../utils/queries';
 import spinner from '../assets/spinner.gif';
 
 function Detail() {
-  const [state, dispatch] = useStoreContext();
-  const { id } = useParams();
+    const [state, dispatch] = useStoreContext();
+    const { id } = useParams();
+    const [currentProduct, setCurrentProduct] = useState({});
+    const { loading, data } = useQuery(QUERY_PRODUCTS);
+    const { products } = state;
 
-  const [currentProduct, setCurrentProduct] = useState({});
+    useEffect(() => {
+        if (products.length) {
+            setCurrentProduct(products.find((product) => product._id === id));
+        } else if (data) {
+            dispatch({
+                type: UPDATE_PRODUCTS,
+                products: data.products,
+            });
+        }
+    }, [products, data, dispatch, id]);
 
-  const { loading, data } = useQuery(QUERY_PRODUCTS);
+    return (
+        <>
+            {currentProduct ? (
+                <div className="container my-1">
+                    <Link to="/">← Back to Products</Link>
 
-  const { products } = state;
+                    <h2>{currentProduct.name}</h2>
 
-  useEffect(() => {
-    if (products.length) {
-      setCurrentProduct(products.find((product) => product._id === id));
-    } else if (data) {
-      dispatch({
-        type: UPDATE_PRODUCTS,
-        products: data.products,
-      });
-    }
-  }, [products, data, dispatch, id]);
+                    <p>{currentProduct.description}</p>
 
-  return (
-    <>
-      {currentProduct ? (
-        <div className="container my-1">
-          <Link to="/">← Back to Products</Link>
+                    <p>
+                        <strong>Price:</strong>${currentProduct.price}{' '}
+                        <button>Add to Cart</button>
+                        <button>Remove from Cart</button>
+                    </p>
 
-          <h2>{currentProduct.name}</h2>
-
-          <p>{currentProduct.description}</p>
-
-          <p>
-            <strong>Price:</strong>${currentProduct.price}{' '}
-            <button>Add to Cart</button>
-            <button>Remove from Cart</button>
-          </p>
-
-          <img
-            src={`/images/${currentProduct.image}`}
-            alt={currentProduct.name}
-          />
-        </div>
-      ) : null}
-      {loading ? <img src={spinner} alt="loading" /> : null}
-    </>
-  );
+                    <img
+                        src={`/images/${currentProduct.image}`}
+                        alt={currentProduct.name}
+                    />
+                </div>
+            ) : null}
+            {loading ? <img src={spinner} alt="loading" /> : null}
+        </>
+    );
 }
 
 export default Detail;
