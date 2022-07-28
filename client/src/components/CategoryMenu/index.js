3f97ac5d28cde163acbfe31dfb154ea212b69612
Ben H <deepfreespace@gmail.com>
2022-07-27 18:42:17 -0600

    Replace files with snapshot

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -1,50 +1,50 @@
 import React, { useEffect } from 'react';
 import { useQuery } from '@apollo/client';
-import { QUERY_CATEGORIES } from '../../utils/queries';
 import { useStoreContext } from '../../utils/GlobalState';
 import {
-    UPDATE_CATEGORIES,
-    UPDATE_CURRENT_CATEGORY,
+  UPDATE_CATEGORIES,
+  UPDATE_CURRENT_CATEGORY,
 } from '../../utils/actions';
+import { QUERY_CATEGORIES } from '../../utils/queries';
 
 function CategoryMenu() {
-    const [state, dispatch] = useStoreContext();
-    const { categories } = state;
-    const { data: categoryData } = useQuery(QUERY_CATEGORIES);
+  const [state, dispatch] = useStoreContext();
 
-    useEffect(() => {
-        // if categoryData exists or has changed from the response of useQuery, then run dispatch()
-        if (categoryData) {
-            // execute our dispatch function with our action object indicating the type of action and the data to set our state for categories
-            dispatch({
-                type: UPDATE_CATEGORIES,
-                categories: categoryData.categories,
-            });
-        }
-    }, [categoryData, dispatch]);
+  const { categories } = state;
 
-    const handleClick = (id) => {
-        dispatch({
-            type: UPDATE_CURRENT_CATEGORY,
-            currentCategory: id,
-        });
-    };
+  const { data: categoryData } = useQuery(QUERY_CATEGORIES);
 
-    return (
-        <div>
-            <h2>Choose a Category:</h2>
-            {categories.map((item) => (
-                <button
-                    key={item._id}
-                    onClick={() => {
-                        handleClick(item._id);
-                    }}
-                >
-                    {item.name}
-                </button>
-            ))}
-        </div>
-    );
+  useEffect(() => {
+    if (categoryData) {
+      dispatch({
+        type: UPDATE_CATEGORIES,
+        categories: categoryData.categories,
+      });
+    }
+  }, [categoryData, dispatch]);
+
+  const handleClick = (id) => {
+    dispatch({
+      type: UPDATE_CURRENT_CATEGORY,
+      currentCategory: id,
+    });
+  };
+
+  return (
+    <div>
+      <h2>Choose a Category:</h2>
+      {categories.map((item) => (
+        <button
+          key={item._id}
+          onClick={() => {
+            handleClick(item._id);
+          }}
+        >
+          {item.name}
+        </button>
+      ))}
+    </div>
+  );
 }
 
 export default CategoryMenu;
